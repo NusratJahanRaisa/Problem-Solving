3a43e33a28a0ed6deaf591234bb0e6344bb828f6
Nusrat Jahan Raisa <jahanthinks@gmail.com>
2025-10-08 14:11:46 +0600

    solved

--- CF_ExtremelyRound.cpp
+++ CF_ExtremelyRound.cpp
@@ -8,40 +8,23 @@ using namespace std;
 int main(){ 
     optimize(); 
     
-    int t;cin>>t;while(t--)
-    {
-        /* code */
-       
-       
-        int n;cin>>n;        
-        int res;
-
-        if(n<10){
-            cout<<n<<endl;
-        }
-
-        else{
-        
-        if(n<=90){
-            res=9+(n/10);
-        }
-        else if(n<=999){
-            res=9+9+(n/100);
-        }
-        else if(n<=9999){
-            res=9+9+9+(n/1000);
-        }
-        else if(n<=99999){
-            res=9+9+9+9+(n/10000);
-        }
-        else if(n<=999999){
-            res=9+9+9+9+9+(n/100000);
-        }
-
-        cout<<res<<endl;
-
-        }
+    int T;
+    cin>>T;
+    while (T--) {
+        int n;
+        cin>>n;
+        int ans=0;
+        for(int firstDig=1;firstDig<=9;firstDig++){
+            int num = firstDig;
+            while (num<=n)
+            {
+                /* code */
+                ans++;
+                num*=10;
+            }
             
+        }
+        cout<<ans<<endl;
     }
     
     
