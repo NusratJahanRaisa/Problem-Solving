d6363b1754f827117ced4a87e874ce5405db5c66
Nusrat Jahan Raisa <jahanthinks@gmail.com>
2026-01-11 00:51:25 +0600

    fractional knapsack greedy approach done

--- Random Problems From Different Platforms/FractionalKnapsack.cpp
+++ Random Problems From Different Platforms/FractionalKnapsack.cpp
@@ -5,6 +5,54 @@
     #define no cout<<"NO\n";
     #define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
     
+
+
+    class Solution {
+  public:
+     
+    static bool comparetor(pair<double,int> &p1, pair<double,int> &p2){ 
+         if(p1.first == p2.first)
+            return p1.second > p2.second; 
+         return p1.first > p2.first;  
+    }
+    
+    double fractionalKnapsack(vector<int>& val, vector<int>& w, int capacity) {
+        // code here
+        vector <pair <double,int>> vp;
+        
+        for(int i=0;i<val.size();i++){
+            double ratio = (double)val[i]/w[i];
+            vp.push_back({ratio,w[i]});
+        }
+        
+        sort(vp.begin(),vp.end(),comparetor);
+        
+        double totalValue=0;
+        double totalWeight=0;
+        
+        for(auto u: vp){
+            
+            // cout<<u.first<<" "<<u.second<<endl;
+            
+            if(u.second>=capacity-totalWeight){
+                totalValue+=((double)(capacity-totalWeight)*u.first);
+                break;
+            }
+            
+            if(totalWeight<capacity){
+                totalValue+=((double)u.second*u.first);
+                totalWeight+=u.second;
+            }
+        }
+        
+        return totalValue;
+    }
+};
+
+
+
+
+
     int main(){ 
         optimize();
         
@@ -13,30 +61,5 @@
         return 0; 
     }
 
+
     
-    double fractionalKnapsack(vector<int>&val, vector<int>& wt, int capacity) {
-        // code here
-        vector <pair <int,int>> vp;
-        for(int i=0;i<val.size();i++){
-            vp.push_back({val[i],wt[i]});
-        }
-        sort(vp.begin(),vp.end());
-        
-        double totalValue=0;
-        double totalWeight=0;
-        
-        for(auto u: vp){
-            cout<<u.first<<" "<<u.second<<endl;
-            
-            if(u.second>=capacity-totalWeight){
-                totalValue+=((u.first*(capacity-totalWeight))/u.second);
-                break;
-            }
-            if(totalWeight<capacity){
-                totalValue+=u.first;
-                totalWeight+=u.second;
-            }
-        }
-        
-        return totalValue;
-    }
