89bf44206f2bc679922e16bd650549056b8b39ae
Nusrat Jahan Raisa <jahanthinks@gmail.com>
2026-01-21 00:11:12 +0600

    solved

---
+++ Codeforces/A_KefaAndFirstSteps.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define yes cout<<"YES\n";
+#define no cout<<"NO\n";
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+void Ja_han(){
+
+    int n; cin >> n;
+    vector<int> v(n);
+
+    for(int i = 0; i < n; i++) cin >> v[i];
+
+    int maxLengthCount = 1;
+    int cnt=1;
+
+    for(int i = 0; i < n-1; i++){
+        if(v[i]<=v[i+1]){
+            cnt++;
+        }else{
+            maxLengthCount=max(cnt,maxLengthCount);
+            cnt=1;
+        }
+    }
+    cout<<max(cnt,maxLengthCount)<<endl;
+}
+
+int main(){
+    optimize();
+
+    int t=1;
+    //  cin >> t;
+    while(t--){
+        Ja_han();
+    }
+
+    return 0;
+}
