3931be02e2b1caaf705cd0f97f5dbaf309f3758c
Nusrat Jahan Raisa <jahanthinks@gmail.com>
2026-01-22 12:30:39 +0600

    could not solve it yesterday but today it took just 3 minutes to understand! Problem solving is mysterious and FUN!

---
+++ Codeforces/A_UploadMoreRAM.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define yes cout<<"YES\n";
+#define no cout<<"NO\n";
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+void Ja_han(){
+
+    int n,k; cin >> n >>k;
+    cout<<(n-1)*k+1<<endl;    
+}
+
+int main(){
+    optimize();
+
+    int t; cin >> t;
+    while(t--){
+        Ja_han();
+    }
+
+    return 0;
+}
