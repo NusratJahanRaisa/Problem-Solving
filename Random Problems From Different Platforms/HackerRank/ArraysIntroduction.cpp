622bd095f912a43047d66f03503b15542fb22724
Nusrat Jahan Raisa <jahanthinks@gmail.com>
2026-01-27 16:58:39 +0600

    solved

---
+++ Random Problems From Different Platforms/HackerRank/ArraysIntroduction.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define yes cout<<"YES\n";
+#define no cout<<"NO\n";
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+void Ja_han(){
+
+    int n; cin >> n;
+    vector<int> v(n);
+
+    for(int i = 0; i < n; i++) cin >> v[i];
+
+    for(int i=n-1;i>=0;i--){
+        cout<<v[i]<<" ";
+    }
+    cout<<endl;
+}
+
+int main(){
+    optimize();
+
+    int t=1;
+    //  cin >> t;
+    while(t--){
+        Ja_han();
+    }
+
+    return 0;
+}
