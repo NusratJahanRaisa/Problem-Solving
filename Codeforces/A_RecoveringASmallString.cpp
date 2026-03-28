3f90a5255d68f8e24bff11655f83e17c710e50bd
Nusrat Jahan Raisa <jahanthinks@gmail.com>
2026-03-28 23:59:55 +0600

    solved

---
+++ Codeforces/A_RecoveringASmallString.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define yes cout<<"YES\n";
+#define no cout<<"NO\n";
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+void Ja_han(){
+
+    int n; cin >> n;
+
+    string s;s="aaa";
+    n-=3;
+    for(int i=2;i>-1;i--)
+    {
+        /* code */
+        int tmp = min(25,n);
+        s[i]+=tmp;
+        n-=tmp;
+    }
+    
+    cout<<s<<endl;
+    
+}
+
+int main(){
+    optimize();
+
+    int t; cin >> t;
+    while(t--){
+        Ja_han();
+    }
+
+    return 0;
+}
